1ba49a27239e9597d0417ad5801c4452d303f2b7
Soab42 <108150913+Soab42@users.noreply.github.com>
2024-02-06 01:37:00 +0600

    init

--- zone/index.js
+++ zone/index.js
@@ -20,49 +20,48 @@ const loginCredentials = [
   { username: "fotulla-1" },
 ];
 
-document.addEventListener("DOMContentLoaded", () => {
-  const jsonDisplay = document.querySelector("tbody");
-  const total_periodical_progress_information = [];
-  let serialNumber = 1;
+const jsonDisplay = document.querySelector("tbody");
+const total_periodical_progress_information = [];
+let serialNumber = 1;
 
-  const fetchData = () => {
-    fetch("http://localhost:3000/api/data")
-      .then((response) => response.json())
-      .then((data) => {
-        showDataTable(data);
-      })
-      .finally(() => {
-        showTotalRow();
-        // console.log(totalRow);
-      })
-      .catch((error) => console.error("Error fetching JSON:", error));
-  };
+const fetchData = () => {
+  fetch("http://localhost:3000/api/data")
+    .then((response) => response.json())
+    .then((data) => {
+      showDataTable(data);
+    })
+    .finally(() => {
+      showTotalRow();
+      // console.log(totalRow);
+    })
+    .catch((error) => console.error("Error fetching JSON:", error));
+};
 
-  fetchData();
+fetchData();
 
-  //   getDataButton.addEventListener("click", fetchData);
+//   getDataButton.addEventListener("click", fetchData);
 
-  function showDataTable(data) {
-    // Display JSON data in HTML
+function showDataTable(data) {
+  // Display JSON data in HTML
 
-    for (const { username } of loginCredentials) {
-      const userData = data[username];
-      const {
-        members = {},
-        savings = {},
-        loans = {},
-      } = userData?.periodical_progress_data?.total_periodical_progress_info;
-      if ((members, savings, loans)) {
-        total_periodical_progress_information.push({
-          ...members,
-          ...savings,
-          ...loans,
-        });
-      }
+  for (const { username } of loginCredentials) {
+    const userData = data[username];
+    const {
+      members = {},
+      savings = {},
+      loans = {},
+    } = userData?.periodical_progress_data?.total_periodical_progress_info;
+    if ((members, savings, loans)) {
+      total_periodical_progress_information.push({
+        ...members,
+        ...savings,
+        ...loans,
+      });
+    }
 
-      // Check if the username exists in the data
-      if (data[username]) {
-        jsonDisplay.innerHTML += `<tr>
+    // Check if the username exists in the data
+    if (data[username]) {
+      jsonDisplay.innerHTML += `<tr>
             <td>${serialNumber}</td>
             <td>${userData.branch_data.name}</td>
             <td>${userData.branch_data.code}</td>
@@ -101,120 +100,111 @@ document.addEventListener("DOMContentLoaded", () => {
             <td>${loans.principle_regular_recovery.toFixed(2)}</td>
 
             </tr>`;
-        serialNumber++;
-      } else {
-        jsonDisplay.innerHTML += `<tr><td colSpan="3">
+      serialNumber++;
+    } else {
+      jsonDisplay.innerHTML += `<tr><td colSpan="3">
             No data found for username: ${username}</td></tr>`;
-      }
     }
   }
+}
 
-  function showTotalRow() {
-    const totalRow = total_periodical_progress_information.reduce(
-      (acc, curr) => {
-        console.log(acc);
-        acc.member_count_previous = checkInitialData(
-          acc.member_count_previous,
-          curr.member_count_previous
-        );
-        acc.member_count_current = checkInitialData(
-          acc.member_count_current,
-          curr.member_count_current
-        );
-        acc.savings_balance_previous = checkInitialData(
-          acc.savings_balance_previous,
-          curr.savings_balance_previous
-        );
-        acc.savings_balance_current = checkInitialData(
-          acc.savings_balance_current,
-          curr.savings_balance_current
-        );
-        acc.loan_outstanding_principal_previous = checkInitialData(
-          acc.loan_outstanding_principal_previous,
-          curr.loan_outstanding_principal_previous
-        );
-        acc.loan_outstanding_principal_current = checkInitialData(
-          acc.loan_outstanding_principal_current,
-          curr.loan_outstanding_principal_current
-        );
-        acc.previous_borrower_member = checkInitialData(
-          acc.previous_borrower_member,
-          curr.previous_borrower_member
-        );
-        acc.current_borrower_member = checkInitialData(
-          acc.current_borrower_member,
-          curr.current_borrower_member
-        );
-        acc.principal_due_count_previous = checkInitialData(
-          acc.principal_due_count_previous,
-          curr.principal_due_count_previous
-        );
-        acc.principal_due_previous = checkInitialData(
-          acc.principal_due_previous,
-          curr.principal_due_previous
-        );
-        acc.current_expired_borrower_count_previous = checkInitialData(
-          acc.current_expired_borrower_count_previous,
-          curr.current_expired_borrower_count_previous
-        );
-        acc.current_expired_principal_amount_previous = checkInitialData(
-          acc.current_expired_principal_amount_previous,
-          curr.current_expired_principal_amount_previous
-        );
-        acc.due_expired_principal_borrower_count_previous = checkInitialData(
-          acc.due_expired_principal_borrower_count_previous,
-          curr.due_expired_principal_borrower_count_previous
-        );
-        acc.due_expired_amount_principal_previous = checkInitialData(
-          acc.due_expired_amount_principal_previous,
-          curr.due_expired_amount_principal_previous
-        );
-        acc.principal_due_count_current = checkInitialData(
-          acc.principal_due_count_current,
-          curr.principal_due_count_current
-        );
-        acc.principal_due_current = checkInitialData(
-          acc.principal_due_current,
-          curr.principal_due_current
-        );
-        acc.current_expired_borrower_count_current = checkInitialData(
-          acc.current_expired_borrower_count_current,
-          curr.current_expired_borrower_count_current
-        );
-        acc.current_expired_principal_amount_current = checkInitialData(
-          acc.current_expired_principal_amount_current,
-          curr.current_expired_principal_amount_current
-        ); // Check the typo in the) original line
-        acc.due_expired_principal_borrower_count_current = checkInitialData(
-          acc.due_expired_principal_borrower_count_current,
-          curr.due_expired_principal_borrower_count_current
-        );
-        acc.due_expired_amount_principal_current = checkInitialData(
-          acc.due_expired_amount_principal_current,
-          curr.due_expired_amount_principal_current
-        );
-        acc.OTR_principal = checkInitialData(
-          acc.OTR_principal,
-          curr.OTR_principal
-        );
-
-        acc.PRA_priciple = checkInitialData(
-          acc.PRA_priciple,
-          curr.PRA_priciple
-        ); // Check the typo in the original) lineacc.PRA_priciple,
-        acc.principle_recoverable = checkInitialData(
-          acc.principle_recoverable,
-          curr.principle_recoverable
-        );
-        acc.principle_regular_recovery = checkInitialData(
-          acc.principle_regular_recovery,
-          curr.principle_regular_recovery
-        );
-        return acc;
-      },
-      {}
+function showTotalRow() {
+  const totalRow = total_periodical_progress_information.reduce((acc, curr) => {
+    console.log(acc);
+    acc.member_count_previous = checkInitialData(
+      acc.member_count_previous,
+      curr.member_count_previous
     );
-    jsonDisplay.innerHTML += `<tr style="background:#45f80e70;font-weight:bold">
+    acc.member_count_current = checkInitialData(
+      acc.member_count_current,
+      curr.member_count_current
+    );
+    acc.savings_balance_previous = checkInitialData(
+      acc.savings_balance_previous,
+      curr.savings_balance_previous
+    );
+    acc.savings_balance_current = checkInitialData(
+      acc.savings_balance_current,
+      curr.savings_balance_current
+    );
+    acc.loan_outstanding_principal_previous = checkInitialData(
+      acc.loan_outstanding_principal_previous,
+      curr.loan_outstanding_principal_previous
+    );
+    acc.loan_outstanding_principal_current = checkInitialData(
+      acc.loan_outstanding_principal_current,
+      curr.loan_outstanding_principal_current
+    );
+    acc.previous_borrower_member = checkInitialData(
+      acc.previous_borrower_member,
+      curr.previous_borrower_member
+    );
+    acc.current_borrower_member = checkInitialData(
+      acc.current_borrower_member,
+      curr.current_borrower_member
+    );
+    acc.principal_due_count_previous = checkInitialData(
+      acc.principal_due_count_previous,
+      curr.principal_due_count_previous
+    );
+    acc.principal_due_previous = checkInitialData(
+      acc.principal_due_previous,
+      curr.principal_due_previous
+    );
+    acc.current_expired_borrower_count_previous = checkInitialData(
+      acc.current_expired_borrower_count_previous,
+      curr.current_expired_borrower_count_previous
+    );
+    acc.current_expired_principal_amount_previous = checkInitialData(
+      acc.current_expired_principal_amount_previous,
+      curr.current_expired_principal_amount_previous
+    );
+    acc.due_expired_principal_borrower_count_previous = checkInitialData(
+      acc.due_expired_principal_borrower_count_previous,
+      curr.due_expired_principal_borrower_count_previous
+    );
+    acc.due_expired_amount_principal_previous = checkInitialData(
+      acc.due_expired_amount_principal_previous,
+      curr.due_expired_amount_principal_previous
+    );
+    acc.principal_due_count_current = checkInitialData(
+      acc.principal_due_count_current,
+      curr.principal_due_count_current
+    );
+    acc.principal_due_current = checkInitialData(
+      acc.principal_due_current,
+      curr.principal_due_current
+    );
+    acc.current_expired_borrower_count_current = checkInitialData(
+      acc.current_expired_borrower_count_current,
+      curr.current_expired_borrower_count_current
+    );
+    acc.current_expired_principal_amount_current = checkInitialData(
+      acc.current_expired_principal_amount_current,
+      curr.current_expired_principal_amount_current
+    ); // Check the typo in the) original line
+    acc.due_expired_principal_borrower_count_current = checkInitialData(
+      acc.due_expired_principal_borrower_count_current,
+      curr.due_expired_principal_borrower_count_current
+    );
+    acc.due_expired_amount_principal_current = checkInitialData(
+      acc.due_expired_amount_principal_current,
+      curr.due_expired_amount_principal_current
+    );
+    acc.OTR_principal = checkInitialData(acc.OTR_principal, curr.OTR_principal);
+
+    acc.PRA_priciple = checkInitialData(acc.PRA_priciple, curr.PRA_priciple); // Check the typo in the original) lineacc.PRA_priciple,
+    acc.principle_recoverable = checkInitialData(
+      acc.principle_recoverable,
+      curr.principle_recoverable
+    );
+    acc.principle_regular_recovery = checkInitialData(
+      acc.principle_regular_recovery,
+      curr.principle_regular_recovery
+    );
+    return acc;
+  }, {});
+  jsonDisplay.innerHTML += `<tr style="background:#45f80e70;font-weight:bold">
    
       <td colspan='3'>Total</td>
       <td>${totalRow.member_count_previous}</td>
@@ -248,53 +238,53 @@ document.addEventListener("DOMContentLoaded", () => {
       <td>${totalRow.principle_regular_recovery.toFixed(2)}</td>
 
       </tr>`;
-    jsonDisplay.innerHTML += calculateChanges(totalRow);
-  }
-  function calculateChanges(totalRow) {
-    const memberCount =
-      totalRow.member_count_current - totalRow.member_count_previous;
+  jsonDisplay.innerHTML += calculateChanges(totalRow);
+}
+function calculateChanges(totalRow) {
+  const memberCount =
+    totalRow.member_count_current - totalRow.member_count_previous;
 
-    const savings_balance =
-      totalRow.savings_balance_current - totalRow.savings_balance_previous;
+  const savings_balance =
+    totalRow.savings_balance_current - totalRow.savings_balance_previous;
 
-    const loan_outstanding_principal =
-      totalRow.loan_outstanding_principal_current.toFixed(2) -
-      totalRow.loan_outstanding_principal_previous.toFixed(2);
+  const loan_outstanding_principal =
+    totalRow.loan_outstanding_principal_current.toFixed(2) -
+    totalRow.loan_outstanding_principal_previous.toFixed(2);
 
-    const borrower =
-      totalRow.current_borrower_member - totalRow.previous_borrower_member;
+  const borrower =
+    totalRow.current_borrower_member - totalRow.previous_borrower_member;
 
-    //  prev
+  //  prev
 
-    const curr_due_count =
-      totalRow.principal_due_count_current -
-      totalRow.principal_due_count_previous;
+  const curr_due_count =
+    totalRow.principal_due_count_current -
+    totalRow.principal_due_count_previous;
 
-    const curr_due_amount =
-      totalRow.principal_due_current.toFixed(2) -
-      totalRow.principal_due_previous.toFixed(2);
+  const curr_due_amount =
+    totalRow.principal_due_current.toFixed(2) -
+    totalRow.principal_due_previous.toFixed(2);
 
-    const exp_due_count =
-      totalRow.current_expired_borrower_count_current -
-      totalRow.current_expired_borrower_count_previous;
+  const exp_due_count =
+    totalRow.current_expired_borrower_count_current -
+    totalRow.current_expired_borrower_count_previous;
 
-    const exp_due_amount =
-      totalRow.current_expired_principal_amount_current.toFixed(2) -
-      totalRow.current_expired_principal_amount_previous.toFixed(2);
+  const exp_due_amount =
+    totalRow.current_expired_principal_amount_current.toFixed(2) -
+    totalRow.current_expired_principal_amount_previous.toFixed(2);
 
-    const total_due_count =
-      totalRow.due_expired_principal_borrower_count_current -
-      totalRow.due_expired_principal_borrower_count_previous;
+  const total_due_count =
+    totalRow.due_expired_principal_borrower_count_current -
+    totalRow.due_expired_principal_borrower_count_previous;
 
-    const total_due_amount =
-      totalRow.due_expired_amount_principal_current.toFixed(2) -
-      totalRow.due_expired_amount_principal_previous.toFixed(2);
+  const total_due_amount =
+    totalRow.due_expired_amount_principal_current.toFixed(2) -
+    totalRow.due_expired_amount_principal_previous.toFixed(2);
 
-    const recoverable =
-      totalRow.principle_recoverable.toFixed(2) -
-      totalRow.principle_regular_recovery.toFixed(2);
+  const recoverable =
+    totalRow.principle_recoverable.toFixed(2) -
+    totalRow.principle_regular_recovery.toFixed(2);
 
-    return `<tr>
+  return `<tr>
         <td colspan="3">
           <div align="center">Changes</div>
         </td>
@@ -407,15 +397,13 @@ document.addEventListener("DOMContentLoaded", () => {
     <td><div align="center"></div></td>
 
       </tr>`;
+}
+function checkInitialData(acc, curr) {
+  if (isNaN(acc)) {
+    return Number(curr);
   }
-  function checkInitialData(acc, curr) {
-    if (isNaN(acc)) {
-      return Number(curr);
-    }
-    return Number(acc + curr);
-  }
-});
-
+  return Number(acc + curr);
+}
 function isPositiveInteger(int) {
   return int > 0;
 }
